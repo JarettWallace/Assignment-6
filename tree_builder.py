ae669e04d635679172d26e43e28a54ad24f8b94d
JarettWallace <Jwallace02@rio.edu>
2025-10-06 01:03:01 -0400

    Add files via upload

--- tree_builder.py
+++ tree_builder.py
@@ -7,8 +7,10 @@ class EmployeeNode:
         right (EmployeeNode): The right child node, representing the right subordinate.
     '''
 
-    # Delete this line and implement the class below
-    pass
+    def __init__(self, name):
+        self.name = name 
+        self.left = None 
+        self.right = None
 
 class TeamTree:
     '''
@@ -21,10 +23,45 @@ class TeamTree:
 
     '''
     
-    # Delete this line and implement the class below
-    pass
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, manager_name, employee_name, side, current_node=None):
+        if current_node is None:
+            current_node = self.root
+        
+        if current_node and current_node.name == manager_name:
+            if side == "left" and current_node.left is None:
+                current_node.left = EmployeeNode(employee_name)
+                print(f"{employee_name} added to the left of {manager_name}.")
+            elif side == "right" and current_node.right is None:
+                current_node.right = EmployeeNode(employee_name)
+                print(f"{employee_name} added to the right of {manager_name}.")
+            else:
+                print(f" The {side} side has already been filled or side is invalid.")
+        elif current_node is not None:
+            if current_node.left:
+                self.insert(manager_name, employee_name, side, current_node.left)
+            if current_node.right:
+                self.insert(manager_name, employee_name, side, current_node.right)
+        else:
+            print(f"Manager {manager_name} could not be found.")
 
-# Test your code here
+    def print_tree(self, node=None, level=0):
+        if node is None:
+            if level == 0:
+                node = self.root
+            else:
+                return
+        print(" " * (level * 2) + f" - {node.name}")
+
+        if node.left:
+            self.print_tree(node.left, level + 1)
+        if node.right:
+            self.print_tree(node.right, level + 1)
+
+    
+    
 
 
 
@@ -34,7 +71,9 @@ class TeamTree:
 
 
 
-# CLI functionality
+
+
+
 def company_directory():
     tree = TeamTree()
 
@@ -69,4 +108,6 @@ def company_directory():
             print("Good Bye!")
             break
         else:
-            print("❌ Invalid option. Try again.")
+            print("❌ Invalid option. Try again.")   
+#design memo should be around 249 words
+#In my code the recursive insertion worked by traversing the tree, during this traverail it was searching for the target manager where the new employee specified should be placed. To further break it down during the course of the search if the current node was already none it would stop the recursion as it would have reached a leaf node that could not go further. When inserting an employee depending if the side that is desired by the user is free, a new employee node would be created and would be added to that side. If the current node was not the target it will search by calling insert on both the left and right subtrees, this will ensure that no matter where the manager is in the tree the method will continue searching the entire tree until the manager is found. The biggest challenges I faced when trying to find the right spot for the new employee where running into various complications such as a side being occupied or a manager not being found, and lastly if the tree were to continue to grow it will become increasingly difficult to run it efficiently cases may arise where there are too many employees on one side compared to the other. The best scenarios where trees would be useful in real examples would be in file systems, indexing a database, even in some basic games such as tic-tac-toe as the tree could represent possible moves for an AI to do. 
